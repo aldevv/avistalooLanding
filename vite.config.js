db85cfaabcfd2066411335dbf0d50a55f6a03c0c
Juan Alejandro Bernal <jbernal@unal.edu.co>
2022-02-09 14:17:03 -0500

    added better config vite

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,10 @@ const mode = process.env.NODE_ENV === "production" ? "production" : "development
 const base = mode === "production" ? "/" + path.basename(process.cwd()) + "/" : "/";
 
 module.exports = {
-  root: "src",
-  base,
-  mode,
-  publicDir: "../public",
-  build: {
-    outDir: "../dist",
-    assetsDir: "./"
-  }
+    root: "src",
+    mode: "production",
+    build: {
+        outDir: "../dist",
+        assetsDir: "./"
+    }
 };
